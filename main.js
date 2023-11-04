19ab95bc3201953e94bf6c5516e832b9683f7dcf
amitesh kumar <amiteshptn@gmail.com>
2023-11-05 01:07:31 +0530

    added wt of water calculations

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 /* parameters for calculations*/
-const top_width=parseInt(document.getElementById('top_width').value);
-const reservoir_width=parseInt(document.getElementById('reservoir_width').value);
-const upper_side_width= parseInt(document.getElementById('upper_side_width').value);
-const lower_side_width=parseInt(document.getElementById('lower_side_width').value);
-const slope= parseInt(document.getElementById('slope').value);
-const tail_water_width= parseInt(document.getElementById('tail_water_width').value);
-const bottom_small_width=parseInt(document.getElementById('bottom_small_width').value);
-const bottom_width= parseInt(document.getElementById('bottom_width').value);
+const top_width=parseFloat(document.getElementById('top_width').value);
+const reservoir_width=parseFloat(document.getElementById('reservoir_width').value);
+const upper_side_width= parseFloat(document.getElementById('upper_side_width').value);
+const lower_side_width=parseFloat(document.getElementById('lower_side_width').value);
+const slope= parseFloat(document.getElementById('slope').value);
+const tail_water_width= parseFloat(document.getElementById('tail_water_width').value);
+const bottom_small_width=parseFloat(document.getElementById('bottom_small_width').value);
+const bottom_width= parseFloat(document.getElementById('bottom_width').value);
 
 function multiplyArrays(arr1, arr2) {
     if (arr1.length !== arr2.length) {
@@ -44,6 +44,32 @@ const w1_lever_arm = (s,b)=>{       // b bottom width t top width s small bottom
     return s/3+b;
 }
 
+// calc of wt of water of dam
+
+const waterWtRect_us = upper_side_width*bottom_small_width*10;
+
+const waterWtTriangle_us = 0.5* bottom_small_width*lower_side_width*10;
+
+const waterWtTriangle_ds = 0.5* tail_water_width* (tail_water_width/slope) *10;
+
+
+// lever arm for weight of water abt toe
+const water_w1_lever_arm = (b,t)=>{       // b bottom small width t bottom width
+    return (b/2)+t;
+}
+const water_w2_lever_arm = (b,t)=>{       // b bottom width t bottom width
+    return (2/3*b)+t;
+}
+const water_w3_lever_arm = (x)=>{       // x tail water width
+    const tailWater = x /slope ;
+    return tailWater/3;
+}
+
+// uplift forces calc
+// ---------------------------------------------------
+
+// constants for changing values in table
+// values of wt of dam
 const wt_lever_val = [w1_lever_arm(bottom_small_width,bottom_width) ,w2_lever_arm(bottom_width,top_width),w3_lever_arm(bottom_width,top_width)]
 
 const lever_listContainer = document.getElementById("wt-lever-arm-items");
@@ -52,14 +78,38 @@ const wt_moment = multiplyArrays(wt_val,wt_lever_val);
 
 const moment_listContainer = document.getElementById("wt-moment-items");
 
+// for water wt
+
+const waterWt_val = [waterWtRect_us,waterWtTriangle_us,waterWtTriangle_ds];
+
+const waterWt = document.getElementById("water-wt");
+
+const waterLeverArm_val =   [water_w1_lever_arm(bottom_small_width,bottom_width),water_w2_lever_arm(bottom_small_width,bottom_width),water_w3_lever_arm(tail_water_width)]
+
+const waterLever_listContainer = document.getElementById("water-wt-lever-arm");
+
+const waterWt_moment = multiplyArrays(waterWt_val,waterLeverArm_val);
+
+const waterMoment_listContainer = document.getElementById("water-wt-moment")
+
+// for uplift forces
+
+
+
+
+
 const calcButton = document.getElementById('button-64');
 
-// to display all wt calc
 calcButton.addEventListener('click',()=>{
 
     lever_listContainer.textContent = '';
     wt_listContainer.textContent = '' ;
     moment_listContainer.textContent = '';
+    waterLever_listContainer.textContent = '';
+    waterMoment_listContainer.textContent = '';
+
+    waterWt.textContent = '';
+// to display all wt calc
 
     wt_val.forEach(item => {
         const wtList = document.createElement("li");
@@ -77,5 +127,29 @@ calcButton.addEventListener('click',()=>{
         moment_listContainer.appendChild(listItem);
     });
 
+// wt Of Water calculations
+    waterWt_val.forEach(item => {
+        const waterWtList = document.createElement("li");
+        waterWtList.textContent = item.toFixed(2);
+        waterWt.appendChild(waterWtList);
+    });
+//     lever arm of water wt
+    waterLeverArm_val.forEach(item => {
+        const waterLeverArmList = document.createElement("li");
+        waterLeverArmList.textContent = item.toFixed(2);
+        waterLever_listContainer.appendChild(waterLeverArmList);
+    });
+//     moment by water wt
+    waterWt_moment.forEach(item => {
+        const a = document.createElement("li");
+        a.textContent = item;
+        waterMoment_listContainer.appendChild(a);
+    });
+// uplift forces
+
 })
+
+
+
+
 
