8e5102a7e35a8a4a09386cfa0171f117dae45945
amitesh kumar <amiteshptn@gmail.com>
2023-11-05 15:06:19 +0530

    added uplift forces calc

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const reservoir_width=parseFloat(document.getElementById('reservoir_width').valu
 const upper_side_width= parseFloat(document.getElementById('upper_side_width').value);
 const lower_side_width=parseFloat(document.getElementById('lower_side_width').value);
 const slope= parseFloat(document.getElementById('slope').value);
-const tail_water_width= parseFloat(document.getElementById('tail_water_width').value);
+const tail_water_depth= parseFloat(document.getElementById('tail_water_width').value);
 const bottom_small_width=parseFloat(document.getElementById('bottom_small_width').value);
 const bottom_width= parseFloat(document.getElementById('bottom_width').value);
 
@@ -50,7 +50,7 @@ const waterWtRect_us = upper_side_width*bottom_small_width*10;
 
 const waterWtTriangle_us = 0.5* bottom_small_width*lower_side_width*10;
 
-const waterWtTriangle_ds = 0.5* tail_water_width* (tail_water_width/slope) *10;
+const waterWtTriangle_ds = 0.5* tail_water_depth* (tail_water_depth/slope) *10;
 
 
 // lever arm for weight of water abt toe
@@ -66,7 +66,15 @@ const water_w3_lever_arm = (x)=>{       // x tail water width
 }
 
 // uplift forces calc
-// ---------------------------------------------------
+const upliftRect = (t,s,b)=>{
+ return - ((t*10*0.6) * (b+s));
+}
+
+const upliftTriangle = (u,l,t,b,s)=>{
+    const upperWaterPressure = 10 * (u + l);
+    const lowerWaterPressure = 10 * t;
+    return -(0.6 * (upperWaterPressure - lowerWaterPressure) * 0.5 * (b+s));
+}
 
 // constants for changing values in table
 // values of wt of dam
@@ -84,7 +92,7 @@ const waterWt_val = [waterWtRect_us,waterWtTriangle_us,waterWtTriangle_ds];
 
 const waterWt = document.getElementById("water-wt");
 
-const waterLeverArm_val =   [water_w1_lever_arm(bottom_small_width,bottom_width),water_w2_lever_arm(bottom_small_width,bottom_width),water_w3_lever_arm(tail_water_width)]
+const waterLeverArm_val =   [water_w1_lever_arm(bottom_small_width,bottom_width),water_w2_lever_arm(bottom_small_width,bottom_width),water_w3_lever_arm(tail_water_depth)]
 
 const waterLever_listContainer = document.getElementById("water-wt-lever-arm");
 
@@ -94,6 +102,27 @@ const waterMoment_listContainer = document.getElementById("water-wt-moment")
 
 // for uplift forces
 
+const upliftForce_val = [upliftRect(tail_water_depth,bottom_small_width,bottom_width),upliftTriangle(upper_side_width,lower_side_width,tail_water_depth,bottom_width,bottom_small_width)];
+
+const uplift_listConatiner =  document.getElementById("uplift-forces");
+
+const upliftLever_val = [(bottom_small_width+bottom_width)/2,(2/3)*(bottom_width+bottom_small_width)]
+
+const upLiftLever_listContainer = document.getElementById('uplift-lever-arm');
+
+const uplift_moment = multiplyArrays(upliftForce_val,upliftLever_val);
+
+const upliftMoment_listContainer = document.getElementById('uplift-moment')
+
+// for upward vertical earthquake forces 0.05W
+
+let upwardVerticalEq_val = 0 ;
+wt_val.forEach(item =>{
+    upwardVerticalEq_val+=item;
+
+})
+
+const earthQuakeVertical = document.getElementById('earthquake-vertical-force');
 
 
 
@@ -107,8 +136,12 @@ calcButton.addEventListener('click',()=>{
     moment_listContainer.textContent = '';
     waterLever_listContainer.textContent = '';
     waterMoment_listContainer.textContent = '';
+    uplift_listConatiner.textContent = '';
+    upLiftLever_listContainer.textContent ='';
+    upliftMoment_listContainer.textContent= '';
 
     waterWt.textContent = '';
+
 // to display all wt calc
 
     wt_val.forEach(item => {
@@ -146,6 +179,26 @@ calcButton.addEventListener('click',()=>{
         waterMoment_listContainer.appendChild(a);
     });
 // uplift forces
+    upliftForce_val.forEach(item => {
+        const upliftList = document.createElement("li");
+        upliftList.textContent = item;
+        uplift_listConatiner.appendChild(upliftList);
+    });
+//     uplift lever arm
+    upliftLever_val.forEach(item => {
+        const upliftLeverList = document.createElement("li");
+        upliftLeverList.textContent = item.toFixed(2);
+        upLiftLever_listContainer.appendChild(upliftLeverList);
+    });
+//     uplift moment arm
+    uplift_moment.forEach(item => {
+        const upliftMomentList = document.createElement("li");
+        upliftMomentList.textContent = item;
+        upliftMoment_listContainer.appendChild(upliftMomentList);
+    });
+// eartquake force
+
+
 
 })
 
